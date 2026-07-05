a64ebb6f3772c2e8fef940ab0e9d483811efe0aa
reprewindai-dev <reprewindai@gmail.com>
2026-07-05 10:58:54 -0400

    Add governed export demo to veklom.dev

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -4,6 +4,7 @@ import DevSidebar from "./components/DevSidebar";
 import WhitepaperContent from "./components/WhitepaperContent";
 import ArchitectureDiagram from "./components/ArchitectureDiagram";
 import HostileAgentDemo from "./components/HostileAgentDemo";
+import { GovernedExportDemo } from "./components/GovernedExportDemo";
 
 export default function DevPortal() {
   return (
@@ -15,8 +16,9 @@ export default function DevPortal() {
 
         {/* Main Content Pane */}
         <div className="flex-1 min-w-0 flex flex-col items-center px-8 lg:px-12 pb-24">
-          <div className="w-full max-w-5xl mt-8">
+          <div className="w-full max-w-5xl mt-8 flex flex-col gap-8">
             <HostileAgentDemo />
+            <GovernedExportDemo />
           </div>
           <div className="w-full border-t border-[#242424] pt-16 mt-8 flex justify-center">
             <WhitepaperContent />
