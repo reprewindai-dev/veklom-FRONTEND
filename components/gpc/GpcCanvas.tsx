b41e1b1661a2d70a65669c7b466413b210758100
reprewindai-dev <reprewindai@gmail.com>
2026-07-08 19:49:51 -0400

    Fix GPC canvas node change typing

--- components/gpc/GpcCanvas.tsx
+++ components/gpc/GpcCanvas.tsx
@@ -26,6 +26,8 @@ import ReactFlow, {
   OnConnect,
   Connection,
   addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 import {
@@ -217,7 +219,7 @@ export const GpcCanvas: React.FC<GpcCanvasProps> = React.memo(
       (changes) => {
         const canvasStore = useCanvasStore.getState();
         changes.forEach((change) => {
-          if (change.type === 'position' && 'position' in change) {
+          if (change.type === 'position' && change.position) {
             canvasStore.moveNode(change.id, change.position);
           } else if (change.type === 'select' && 'selected' in change) {
             if (change.selected) {
@@ -225,7 +227,7 @@ export const GpcCanvas: React.FC<GpcCanvasProps> = React.memo(
             }
           }
         });
-        setCanvasNodes(changes);
+        setCanvasNodes((nodes) => applyNodeChanges(changes, nodes));
       },
       [setCanvasNodes]
     );
@@ -238,7 +240,7 @@ export const GpcCanvas: React.FC<GpcCanvasProps> = React.memo(
             canvasStore.deleteEdge(change.id);
           }
         });
-        setCanvasEdges(changes);
+        setCanvasEdges((edges) => applyEdgeChanges(changes, edges));
       },
       [setCanvasEdges]
     );
