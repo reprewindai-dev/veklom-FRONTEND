058e3488ac9ead684952fa7b659578800c088ac6
reprewindai-dev <reprewindai@gmail.com>
2026-08-17 23:44:27 -0400

    chore: Remove scratch files

--- rewrite_page_3.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('app/page.tsx', 'utf8');
-
-// Remove grayscale
-content = content.replace(/data-\[machine=true\]:grayscale/g, '');
-
-fs.writeFileSync('app/page.tsx', content, 'utf8');
