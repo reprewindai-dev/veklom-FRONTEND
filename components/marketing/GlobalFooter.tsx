d828e9d5ba203cdd6b25c85ecc16bf1028082ceb
reprewindai-dev <reprewindai@gmail.com>
2026-09-24 08:18:54 -0400

    feat(seo): link consequence authority from marketing footer

--- components/marketing/GlobalFooter.tsx
+++ components/marketing/GlobalFooter.tsx
@@ -14,6 +14,7 @@ const groups = [
     links: [
       
       ["/lockerphycer", "LockerPhycer"],
+      ["/consequence-authority", "Consequence Authority"],
       ["/cappo", "CAPPO"],
       ["/capi", "cAPI"],
       ["/vlink", "VLink"],
